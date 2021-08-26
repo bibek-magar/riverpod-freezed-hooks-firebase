2e80fb972e0714c648a9ab212adf4a5222acc6f1
beevekmgr <beevekmgr@gmail.com>
2021-08-26 08:46:33 +0545

    feat: Added freezed

--- lib/modals/item_model.dart
+++ lib/modals/item_model.dart
@@ -1,10 +1,12 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
 import 'package:flutter/foundation.dart';
 
 //toString hashcode equality
-part 'item_model.freezed.dart';
 //From JSON & to JSON
-part 'item.model.g.dart';
+
+part 'item_model.freezed.dart';
+part 'item_model.g.dart';
 
 @freezed
 abstract class Item with _$Item {
@@ -14,5 +16,12 @@ abstract class Item with _$Item {
     @Default(false) bool obtained,
   }) = _Item;
 
+  factory Item.empty() => Item(name: '');
+
   factory Item.fromJson(Map<String, dynamic> json) => _$ItemFromJson(json);
+
+  factory Item.fromDocument(DocumentSnapshot doc) {
+    final data = doc.data()!;
+    return Item.fromJson(data).copyWith(id: doc.id);
+  }
 }
